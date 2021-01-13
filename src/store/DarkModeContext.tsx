1f6827bc5d2a7965036a6a068af70c371278568c
Abner Rodrigues <abnerluis1001@gmail.com>
2021-01-13 10:50:15 -0300

    feat: added dark class to body

--- src/store/DarkModeContext.tsx
+++ src/store/DarkModeContext.tsx
@@ -3,7 +3,8 @@ import {
   DispatchWithoutAction,
   ReactElement,
   ReactNode,
-  ReactNodeArray
+  ReactNodeArray,
+  useEffect
 } from 'react'
 
 import useToggle from '@hooks/useToggle'
@@ -21,6 +22,10 @@ export const DarkModeContext = createContext<IDarkMode | null>(null)
 
 export default function DarkModeProvider({ children }: Props): ReactElement {
   const [darkMode, toggleDarkMode] = useToggle()
+
+  useEffect(() => {
+    document.body.classList.toggle('dark', darkMode)
+  }, [darkMode])
 
   return (
     <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
