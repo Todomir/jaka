1a14a09ab5cd9cb323c06f6df59cc505f25d2a6d
Abner Rodrigues <abnerluis1001@gmail.com>
2021-01-12 23:36:26 -0300

    style: improved contrast on buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -22,15 +22,15 @@ export default function Button({
   const renderVariantClasses = () => {
     if (outlined) {
       if (color === 'primary') {
-        return 'bg-white border border-indigo-300 border-opacity-75 text-indigo-300 hover:border-indigo-400 hover:text-indigo-400 hover:bg-indigo-50 dark:bg-gray-800'
+        return 'bg-white border border-indigo-500 border-opacity-75 text-indigo-500 hover:border-indigo-600 hover:text-indigo-600 hover:bg-indigo-50 dark:bg-gray-800 dark:border-indigo-400 dark:text-indigo-300 dark:hover:border-indigo-300 dark:hover:text-indigo-200'
       } else {
-        return 'bg-white border border-purple-300 border-opacity-75 text-indigo-300 hover:border-purple-400 hover:text-purple-400 hover:bg-purple-50 dark:bg-gray-800'
+        return 'bg-white border border-purple-500 border-opacity-75 text-indigo-500 hover:border-purple-600 hover:text-purple-600 hover:bg-purple-50 dark:bg-gray-800 dark:border-purple-400 dark:text-purple-300 dark:hover:border-purple-300 dark:hover:text-purple-200'
       }
     } else {
       if (color === 'primary') {
-        return 'bg-indigo-300 text-white hover:bg-indigo-400'
+        return 'bg-indigo-500 text-white hover:bg-indigo-600'
       } else {
-        return 'bg-purple-300 text-white hover:bg-purple-400'
+        return 'bg-purple-500 text-white hover:bg-purple-600'
       }
     }
   }
@@ -40,7 +40,7 @@ export default function Button({
       className={`flex justify-center items-center px-4 py-2 min-w-70 font-semibold transition-colors ${renderVariantClasses()} ${
         small ? 'text-sm' : 'text-md'
       } ${className} focus:outline-none focus:ring-2 focus:ring-offset-2 focus:${
-        color === 'primary' ? 'ring-indigo-300' : 'ring-purple-300'
+        color === 'primary' ? 'ring-indigo-500' : 'ring-purple-500'
       } dark:ring-offset-gray-900`}
       {...props}
     >
