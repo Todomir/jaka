6f5588c0ef0e11752ebc75173c74fb3cee6f1628
Abner Rodrigues <abnerluis1001@gmail.com>
2021-01-12 23:35:54 -0300

    feat: improved navbar responsiveness

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -12,17 +12,17 @@ export default function Nav(): ReactElement {
   return (
     <nav>
       <section className="px-10 py-5 fixed w-full bg-white flex justify-between items-center dark:bg-gray-900 z-50">
-        <div className="flex justify-center items-center space-x-4">
-          <Image src="/logo.svg" width={24} height={24} />
-          <p className="h-6 font-black font-cursive text-xl tracking-tighter dark:text-white">
+        <div className="w-full sm:w-auto flex justify-between sm:justify-start items-center space-x-4">
+          <Image src="/logo.svg" layout="fixed" width={24} height={24} />
+          <p className="h-6 hidden sm:block font-black font-cursive text-xl tracking-tighter dark:text-white">
             JAKA
           </p>
           <Switch value={darkMode} toggle={toggleDarkMode} darkMode />
         </div>
 
-        <div className="flex space-x-10">
+        <div className="hidden sm:flex space-x-10">
           <Button label="Login" color="primary" small />
-          <Button label="Register now" color="primary" outlined small />
+          <Button label="Sign in" color="primary" outlined small />
         </div>
       </section>
     </nav>
