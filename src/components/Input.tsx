8df0eaf46323deebf7518a6b3dbdb124d15fafe2
Abner Rodrigues <abnerluis1001@gmail.com>
2021-01-14 11:32:25 -0300

    feat: added react-hook-form error handling and ref forwarding to Input
    BREAKING CHANGE: input component requires a name prop to be passed

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,30 +1,41 @@
-import { ChangeEvent, HTMLProps, ReactElement } from 'react'
+import { ChangeEvent, forwardRef, HTMLProps } from 'react'
+import { DeepMap, FieldError } from 'react-hook-form'
 
 interface InputProps extends HTMLProps<HTMLInputElement> {
   label: string
   value?: string | number
+  name: string
   onChange?: (event: ChangeEvent<HTMLInputElement>) => void
+  errors?: DeepMap<Record<string, unknown>, FieldError>
 }
 
-export default function Input({
-  label,
-  value,
-  onChange,
-  ...props
-}: InputProps): ReactElement {
+const Input = (
+  { label, value, name, onChange, errors, ...props }: InputProps,
+  ref
+) => {
   return (
     <div className="flex flex-col space-y-2 mb-4">
-      <label className="font-semibold" htmlFor={label}>
+      <label className="font-semibold" htmlFor={name}>
         {label}
       </label>
       <input
         {...props}
-        className="py-2 px-3 border border-gray-200 shadow-sm rounded focus:outline-none focus:ring-1 focus:ring-indigo-400"
+        className={`py-2 px-3 border ${
+          errors[name] ? 'border-red-600' : 'border-gray-200'
+        } shadow-sm rounded focus:outline-none focus:ring-1 focus:${
+          errors[name] ? 'ring-red-600' : 'ring-indigo-400'
+        }`}
         value={value}
         onChange={onChange}
-        name={label}
-        id={label}
+        name={name}
+        id={name}
+        ref={ref}
       />
+      {errors[name] && (
+        <p className="text-sm text-red-600">{errors[name].message}</p>
+      )}
     </div>
   )
 }
+
+export default forwardRef(Input)
