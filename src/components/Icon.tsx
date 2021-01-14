b727bf32e6d0f1654fe40a69bc106588fe218f61
Abner Rodrigues <abnerluis1001@gmail.com>
2021-01-14 16:35:28 -0300

    feat(icon): added plus icon to Icon component

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -6,7 +6,8 @@ const icons = {
   logout:
     'M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1',
   signin:
-    'M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1'
+    'M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1',
+  plus: 'M12 6v6m0 0v6m0-6h6m-6 0H6'
 }
 
 interface IconProps {
