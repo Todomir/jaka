421fd500e48c3bf96457e95350037018831390a7
Abner Rodrigues <abnerluis1001@gmail.com>
2021-01-20 15:02:08 -0300

    feat: added custom _document.tsx file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,31 +18,6 @@ export default function MyApp({
     <>
       <Head>
         <title>JAKA | Just Another Kanban App</title>
-        <meta charSet="utf-8" />
-        <meta name="description" content="Just Another Kanban App" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Fugaz+One&family=Inter:wght@400;600;700&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <QueryClientProvider client={client}>
         <Hydrate state={pageProps.dehydratedState}>
