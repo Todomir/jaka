4ff61ac54b38b147a57f25d5a74df4d31cba328e
Abner Rodrigues <abnerluis1001@gmail.com>
2021-01-20 13:01:59 -0300

    feat(profile): made profile page responsive

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -79,13 +79,13 @@ export default function Profile({
   }
 
   return (
-    <div className="flex justify-center items-center w-screen h-screen tracking-tight">
-      <div className="bg-gray-100 rounded-lg px-10 pb-14 pt-10 w-7/12 shadow-lg border border-gray-300 dark:bg-gray-800 dark:border-gray-600">
-        <header className="text-4xl font-bold tracking-tighter mb-12">
-          <div className="flex justify-between items-center">
-            My profile
+    <div className="flex px-5 justify-center items-center w-screen h-screen tracking-tight">
+      <div className="bg-gray-100 rounded-lg px-10 pb-14 pt-10 w-full sm:w-7/12 shadow-lg border border-gray-300 dark:bg-gray-800 dark:border-gray-600">
+        <header className="text-4xl sm:text-4xl font-bold tracking-tighter mb-12">
+          <div className="flex flex-col-reverse justify-start items-start sm:flex-row sm:justify-between sm:items-center">
+            <h1 className="mt-5">My profile</h1>
             <Link href="/dashboard">
-              <a className="flex text-base items-center font-semibold tracking-tight transition-colors duration-300 text-indigo-600 hover:text-indigo-500 dark:text-indigo-300 dark:hover:text-indigo-400">
+              <a className="flex items-center font-semibold tracking-tight transition-colors duration-300 text-xs sm:text-sm md:text-base text-indigo-600 hover:text-indigo-500 dark:text-indigo-300 dark:hover:text-indigo-400">
                 <Icon size={18} icon="arrowLeft" />{' '}
                 <span className="ml-3">Return to dashboard</span>
               </a>
@@ -106,7 +106,7 @@ export default function Profile({
               name="password"
             />
 
-            <footer className="-mx-10 -mb-14 py-5 px-7 mt-12 flex justify-between space-x-5 bg-indigo-50 border-t border-gray-300 rounded-b-lg dark:bg-gray-700 dark:border-gray-600">
+            <footer className="-mx-10 -mb-14 py-5 px-7 mt-12 flex flex-col sm:flex-row justify-between space-y-5 sm:space-y-0 sm:space-x-5 bg-indigo-50 border-t border-gray-300 rounded-b-lg dark:bg-gray-700 dark:border-gray-600">
               <Button
                 loading={loading}
                 loadingMessage="Updating information"
