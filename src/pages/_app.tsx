c6289162018b33ef1bf912cd92339d3b01bcf37e
Abner Rodrigues <abnerluis1001@gmail.com>
2021-01-20 14:41:57 -0300

    feat: added favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 import { ReactElement } from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
@@ -14,14 +15,39 @@ export default function MyApp({
   const client = new QueryClient()
 
   return (
-    <QueryClientProvider client={client}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <DarkModeProvider>
-          <ToastProvider>
-            <Component {...pageProps} />
-          </ToastProvider>
-        </DarkModeProvider>
-      </Hydrate>
-    </QueryClientProvider>
+    <>
+      <Head>
+        <title>JAKA | Just Another Kanban App</title>
+        <meta charSet="utf-8" />
+        <meta name="description" content="Just Another Kanban App" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <QueryClientProvider client={client}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <DarkModeProvider>
+            <ToastProvider>
+              <Component {...pageProps} />
+            </ToastProvider>
+          </DarkModeProvider>
+        </Hydrate>
+      </QueryClientProvider>
+    </>
   )
 }
